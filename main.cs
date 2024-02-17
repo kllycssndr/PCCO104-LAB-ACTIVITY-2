caf464a81558e63fbe030d626bb3fe9b5ab48581
kllycssndr <158656855+kllycssndr@users.noreply.github.com>
2024-02-17 02:32:40 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,26 @@
+ using System;
+
+ class Program
+    {
+        public static void Main(string[] args)
+        {
+            string output = "";
+
+            Console.WriteLine("Enter Something or type 'exit' to quit:");
+
+            string input = "";
+            do
+            {
+                Console.Write("Enter Something: ");
+                input = Console.ReadLine();
+
+                if (input.ToLower() != "exit")
+                {
+                    output += input + " ";
+                    Console.WriteLine(output);
+                }
+            } while (input.ToLower() != "exit");
+
+            Console.WriteLine("\nClosing Program...");
+        }
+    }
